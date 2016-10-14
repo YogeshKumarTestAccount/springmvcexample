c1a551ba6d6e08e1f932729135c1624e326de9aa
Yogesh Kumar <yogeshkmr2013@gmail.com>
2016-10-15 00:03:44 +0530

    Initial Commit for WEB MVC Project with maven

---
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.howtodoinjava.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.howtodoinjava.demo.service.EmployeeManager;
+
+@Controller
+@RequestMapping("/employee-module1")
+public class EmployeeController 
+{
+	@Autowired
+	EmployeeManager manager;
+
+	@RequestMapping(value = "/getAllEmployees2", method = RequestMethod.GET)
+	public String getAllEmployees(Model model)
+	{
+		model.addAttribute("employees1", manager.getAllEmployees());
+		
+		return "employeesListDisplay";
+	}
+}
